9d905950174fdb717fd1112b3852368dfaa11490
josemariasaldana <jsaldana@unizar.es>
2016-03-02 13:23:53 +0100

    Add a new openflow rule
    This rule establishes the path for the control traffic between the
    controller and the agent (port 6777)

--- scripts_start_ap_odin/script_start_click.sh
+++ scripts_start_ap_odin/script_start_click.sh
@@ -2,5 +2,7 @@
 sleep 3
 ifconfig ap up
 ovs-vsctl --db=tcp:192.168.1.5:6632 add-port br0 ap
+sleep 3
+ovs-ofctl add-flow br0 in_port=1,dl_type=0x0800,nw_src=192.168.1.129,nw_dst=192.168.1.5,nw_proto=6,tp_dst=6777,actions=output:LOCAL
 
 
